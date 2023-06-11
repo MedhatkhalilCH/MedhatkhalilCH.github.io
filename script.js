d0b4386197e49f0035d811c882639f1c47a01861
Medhat Khalil <medhat.khalil@bluewin.ch>
2023-06-11 14:55:38 +0200

    added more products

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Get all tab links
-    var tabLinks = document.querySelectorAll('.tab-link');
-  
-    // Add event listener to each tab link
-    tabLinks.forEach(function(link) {
-      link.addEventListener('click', function(event) {
-        event.preventDefault();
-        
-        // Get the target tab content ID from the data attribute
-        var tabId = this.getAttribute('data-tab');
-  
-        // Hide all tab contents
-        var tabContents = document.querySelectorAll('.tab-content');
-        tabContents.forEach(function(content) {
-          content.style.display = 'none';
-        });
-  
-        // Show the selected tab content and activate the tab link
-        document.getElementById(tabId).style.display = 'block';
-        this.classList.add('active');
+  // Get all tab links
+  var tabLinks = document.querySelectorAll('.tab-link');
+
+  // Add event listener to each tab link
+  tabLinks.forEach(function(link) {
+    link.addEventListener('click', function(event) {
+      event.preventDefault();
+
+      // Get the target tab content ID from the data attribute
+      var tabId = this.getAttribute('data-tab');
+
+      // Hide all tab contents
+      var tabContents = document.querySelectorAll('.tab-content');
+      tabContents.forEach(function(content) {
+        content.style.display = 'none';
       });
+
+      // Show the selected tab content and activate the tab link
+      document.getElementById(tabId).style.display = 'block';
+      this.classList.add('active');
     });
   });
-  
+
+  // Activate the default tab
+  var defaultTab = document.querySelector('.tab-link[data-tab="grissini"]');
+  defaultTab.classList.add('active');
+  var defaultTabContent = document.getElementById('grissini');
+  defaultTabContent.style.display = 'block';
+});
