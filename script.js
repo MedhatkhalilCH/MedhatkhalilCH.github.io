5f5a1840d1bf2fda7ec4967cef7744a298edca9a
Medhat Khalil <medhat.khalil@bluewin.ch>
2023-06-11 19:50:38 +0200

    adjusted image

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-import translations from './translations.js';
-
 document.addEventListener('DOMContentLoaded', function() {
   // Get all tab links
   var tabLinks = document.querySelectorAll('.tab-link');
@@ -60,6 +58,64 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // Function to translate paragraphs
   function translateParagraphs(language) {
+
+    const translations = {
+      en: {
+          about_paragraph: `Comestibles Zürich invites you on a gastronomic journey, bringing you a curated selection of international flavors.<br>
+              Indulge in the art of fine dining with our handpicked delicacies, sourced from the world's culinary hotspots.<br><br>
+              We source our products from the world-renowned Patti bakery. Francesco Patti moved from Sicily to the Aosta Valley to learn how to become a baker.<br>
+              Then he met Andrea Cavallari, the current head of the Patti bakery, and they started a family business more than 30 years ago.<br>
+              The secret of the success of Patti products comes from a combination of various factors: the choice of genuine top-quality ingredients, passion, craftsmanship, and a dough worked by hand, with love and patience, without preservatives and chemical additives.<br>
+              Patti now has more than 50 employees and several agents, with three stores in the Biella area and a store in Milan.<br>
+              The exquisite breadsticks and pastry products (Baci di dama, Amaretti morbidi, Canestrelli canavesani, Frollini with pine nuts, Torcetti with butter and Paste di meliga) are appreciated and distributed throughout northern Italy, up to overcome the borders reaching some selected shops in Europe.<br>`,
+          grissini_paragraph: `All the Grissinis are made by hand, pulled to the edges to make them longer and thinner.<br>
+              Leavened 3 hours before being baked, the breadsticks of Panificio Patti combine naturalness and taste. That is the way how we get the crunchy and fragrant product.<br>
+              We have 3 types: sesame, natural and butter (with top quality butter from the Netherlands).
+              You can have the natural and the sesame with “farina interna”- is a special flour which reduced glycemic peak by 50 %`,
+          sweets_paragraph: `Discover our delightful selection of sweets`,
+          Torcetti_paragraph: `Is a butter cookie made with leftover dough from the breadsticks adding butter and sugar and than curve it.`,
+          Baci_di_Dama_paragraph: `Is a cookie made by flour and haselnut coming from the Langhe IGP and chocolate in the middle.`,
+          Amaretti_paragraph: `Is a cookie made with almonds and egg whites.`,
+          Paste_di_Meliga_paragraph: `Is a cookie made with corn flour.`,
+          Canestrelli_Canavesani_paragraph: `They are very thin cookies made by cast iron plates.  You can find them in Chocolate, vanille and cofee flavor.`,
+          Canestrelli_Biellesi_paragraph: `Secret recipe, only the Patty bakery is allowed to make this biscuit.<br>
+              Is a wafer with a chocolate ganache in the middle.`,
+          Frollini_al_pinoli_paragraph: `Is a shortbread with pine nuts.`,
+          Panettone_paragraph: `This Piemont Type Christmas cake has a crusty top from haselnut, almond and sugar, filled with cendied fruits and raisins.`,
+          Panettone_al_calvados_paragraph: `Is a panettone cooked sous vide in a jar. It lasts 6 months fresh as just made.<br>
+              The rainins are poached in Calvados.`,
+          Colomba_paragraph: `Made for Easter similar like Panettone, but without raisins and has a bird shape.`,
+          Baba_paragraph: `Two flavor, with limoncello and rum. It is cooked in the jar. Typical sweet from Naples.`,
+      },
+      de: {
+          about_paragraph: `Comestibles Zürich lädt Sie auf eine gastronomische Reise ein und bietet Ihnen eine kuratierte Auswahl an internationalen Aromen.<br>
+              Gönnen Sie sich die Kunst des feinen Essens mit unseren handverlesenen Köstlichkeiten aus den kulinarischen Hotspots der Welt.<br><br>
+              Wir beziehen unsere Produkte von der weltbekannten Bäckerei Patti. Francesco Patti zog von Sizilien ins Aostatal, um Bäcker zu werden.<br>
+              Dann lernte er Andrea Cavallari, den jetzigen Leiter der Patty-Bäckerei kennen, und sie gründeten vor mehr als 30 Jahren ein Familienunternehmen.<br>
+              Das Geheimnis des Erfolgs der Patti-Produkte liegt in einer Kombination verschiedener Faktoren: der Auswahl echter Zutaten von höchster Qualität, Leidenschaft, Handwerkskunst und einem Teig, der mit Liebe und Geduld von Hand verarbeitet wird, ohne Konservierungsstoffe und chemische Zusätze.<br>
+              Patti hat heute mehr als 50 Mitarbeiter und mehrere Agenten, mit drei Filialen in der Gegend von Biella und einer Filiale in Milan.<br>
+              Die exquisiten Grissini und Konditoreiprodukte (Baci di dama, Amaretti morbidi, Canestrelli canavesani, Frollini mit Pinienkernen, Torcetti mit Butter und Paste di meliga) werden in ganz Norditalien geschätzt und vertrieben, bis hin zu einigen ausgewählten Geschäften in Europa.<br>`,
+          grissini_paragraph: `Alle Grissinis werden von Hand gefertigt und an den Enden gezogen, um sie länger und dünner zu machen.
+              Sie sind 3 Stunden vor dem Backen gesäuert, vereinen die Grissini von Panificio Patti Natürlichkeit und Geschmack. Auf diese Weise erhalten wir das knusprige und duftende Produkt.
+              Wir haben 3 Sorten: Sesam, Natur und Butter (mit hochwertiger Butter aus den Niederlanden).
+              Sie können das natürliche und den Sesam mit “Farina interna “- ist ein spezielles Mehl, das die glykämischen Spitzenwerte um 50 % reduziert`,
+          sweets_paragraph: `Entdecken Sie unsere köstliche Auswahl an Süßigkeiten`,
+          Torcetti_paragraph: `Ist ein Butterkeks, der aus Resten des Grissini-Teigs hergestellt wird, Butter und Zucker hinzufügt und dann geformt.`,
+          Baci_di_Dama_paragraph: `Ist ein Keks aus Mehl, Haselnuss (aus der Langhe IGP) und Schokolade in der Mitte`,
+          Amaretti_paragraph: `Ist ein Keks aus Mandeln und Eiweiss`,
+          Paste_di_Meliga_paragraph: `Ist ein Keks aus Maismehl`,
+          Canestrelli_Canavesani_paragraph: `Es sind sehr dünne Kekse, die mit gusseisernen Platten hergestellt werden. Schokoladen-, Vanille- und Cofee-Geschmack.`,
+          Canestrelli_Biellesi_paragraph: `Geheimrezept, nur die Bäckerei Patty darf diesen Keks herstellen.<br>
+              Ist eine Waffel mit einer Schokoladenganache in der Mitte.`,
+          Frollini_al_pinoli_paragraph: `Ist ein Mürbeteig mit Pinienkernen`,
+          Panettone_paragraph: `Dieser piemontesische Weihnachtskuchen hat einen knusprigen Kuchen aus Haselnuss, Mandeln und Zucker, gefüllt mit kandierten Früchten und Rosinen.<br>
+          Für Ostern ähnlich wie Panettone gemacht, aber ohne Rosinen und hat eine Vogelform.`,
+          Panettone_al_calvados_paragraph: `Ist ein Panettone, der in einem Gefäss Sous-Vide gekocht wird. Es hält 6 Monate frisch wie gerade hergestellt.<br>
+              Die Rosinen werden im Calvados gebadet.`,
+          Colomba_paragraph: `Für Ostern hergestellt, ähnlich wie Panettone, jedoch ohne Rosinen und in Vogelform.`,
+          Baba_paragraph: `Zwei Geschmacksrichtungen, mit Limoncello und Rum. Das gleiche wird im Glas gekocht. Typische Süßigkeit aus Neapel `,
+      },
+    };
 
     // Get all paragraph elements
     const paragraphs = document.querySelectorAll('p');
