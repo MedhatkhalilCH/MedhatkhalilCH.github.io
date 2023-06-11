300ab0b4051036119ea4df0d2ff8d6969532143f
Medhat Khalil <medhat.khalil@bluewin.ch>
2023-06-11 14:25:10 +0200

    fixed JS code

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Get all tab links
+    var tabLinks = document.querySelectorAll('.tab-link');
+  
+    // Add event listener to each tab link
+    tabLinks.forEach(function(link) {
+      link.addEventListener('click', function(event) {
+        event.preventDefault();
+        
+        // Get the target tab content ID from the data attribute
+        var tabId = this.getAttribute('data-tab');
+  
+        // Hide all tab contents
+        var tabContents = document.querySelectorAll('.tab-content');
+        tabContents.forEach(function(content) {
+          content.style.display = 'none';
+        });
+  
+        // Deactivate all tab links
+        tabLinks.forEach(function(link) {
+          link.classList.remove('active');
+        });
+  
+        // Show the selected tab content and activate the tab link
+        document.getElementById(tabId).style.display = 'block';
+        this.classList.add('active');
+      });
+    });
+  });
+  
